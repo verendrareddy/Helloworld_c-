d3590dcb448380f6a09a94bf092a0595e227be8e
verendra <verendrareddy.kvr@gmail.com>
2024-09-29 22:53:30 +0530

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication //namspace declararation
+{
+    public class MyClass // clas declaaration
+    {
+        int i = 9; //data member of class
+
+        private int rollno = 20;
+
+        int a, b;
+        public  MyClass(int x, int y)
+        {
+            a = x;
+            b=y;
+        }
+        public  MyClass(MyClass m)
+        {
+            a = m.a;
+            b = m.b;
+        }
+
+        public int RollNo
+        {
+            get
+            {
+                return rollno;
+            }
+
+            set
+            {
+                rollno = value;
+            }
+        }
+
+       
+        
+        
+        public void PrintNum()
+        {
+            Console.WriteLine("Rollnum is {0}", rollno);
+        }
+        public void HelloWorld()  //method defintiion
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        public void Datatype()
+        {
+            int a;
+            double d = 34.567;
+            a = (int)d;
+            Console.WriteLine(a);
+
+            short s = 90;
+            int c;
+            c = s;
+            Console.WriteLine(c);
+
+
+
+            
+        }
+
+
+    }
+    public class Myclass2
+    {
+       
+        
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorldApplication;
+
+MyClass myClass = new MyClass(10,23);
+MyClass myClass1 = new MyClass(myClass);
+myClass.HelloWorld();
+Console.ReadKey();
+
+Console.WriteLine("rollno{0}", myClass.RollNo);
+
